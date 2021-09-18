9c7066f5884502bb414663d522aa2258e4e06f99
Smolldo <maximqwerbich@gmail.com>
2021-09-18 11:56:05 +0300

    6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,12 @@ const valInput = document.querySelector('#validation-input');
 
 const onInputBlur = (event) => {
 event.currentTarget.value.length !== Number(valInput.dataset.length) ?
-valInput.classList.add('invalid') : valInput.classList.remove('invalid', 'valid');
+toggle('invalid', 'valid') : toggle('valid', 'invalid');
 }
 
 valInput.addEventListener('blur', onInputBlur);
+
+const toggle = (add, remove) =>{
+    valInput.classList.remove(remove);
+    valInput.classList.add(add);
+}
